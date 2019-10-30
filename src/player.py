744d865ab24fa244b21ac48a3e9330a5f4d39150
Melanie Harris <mell.j.harris@gmail.com>
2019-10-30 14:25:47 -0700

    room.py completed

--- src/player.py
+++ src/player.py
@@ -5,8 +5,13 @@ class Players:
     def __init__(self, player, room): 
         self.player = player
         self.room = room  
-    def __str__(self):
+    def __str__(self): #user use
         return f"Player: {self.player}, Room: {self.room}"
-    def __repr__(self,):
+    def __repr__(self,): #computer use
         return f"Player: {self.player}, Room: {self.room}"
+
+player = Players("Melanie", "foyer")
+
+# print(Players)
+print(player)
 
--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Rooms:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):  # user use
+        return f"Room: {self.name}, Description: {self.description}"
+
+    def __repr__(self,):  # computer use
+        return f"Room: {self.name}, Description: {self.description}"
+
+
+room = Rooms("foyer", "Dim light filters in from the south. Dusty passages run north and east.")
+
+print(room)
