0a301d78508409e6bd8de185436e255fac446edf
Melanie Harris <mell.j.harris@gmail.com>
2019-10-30 13:30:27 -0700

    working on class for player info

--- src/adv.py
+++ src/adv.py
@@ -38,6 +38,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+newPlayer = room("outside")
 
 # Write a loop that:
 #
--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Players:
+    def __init__(self, player, room): 
+        self.player = player
+        self.room = room  
+    def __str__(self):
+        return f"Player: {self.player}, Room: {self.room}"
+    def __repr__(self,):
+        return f"Player: {self.player}, Room: {self.room}"
+
